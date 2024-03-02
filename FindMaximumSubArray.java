8477c52c738f7ab6625e64a33ac7680a0d0c7a98
Angela Fish <91239079+staradventure@users.noreply.github.com>
2024-03-02 18:53:28 +0800

    更新最大子数组问题。

--- FindMaximumSubArray.java
+++ FindMaximumSubArray.java
@@ -1,3 +1,61 @@
 public class FindMaximumSubArray {
+    public static int[] FIND_MAX_CROSSING_SUBARRAY(int[] A,int low,int mid,int high){
+        int sum=0;
+        int left_sum=Integer.MIN_VALUE;
+        int max_left=-1;
+        for(int i=mid;i>=low;i--){
+            sum+=A[i];
+            if(sum>left_sum){
+                left_sum=sum;
+                max_left=i;
+            }
+        }
+        sum=0;
+        int right_sum=Integer.MIN_VALUE;
+        int max_right=-1;
+        for(int j=mid+1;j<=high;j++){
+            sum+=A[j];
+            if(sum>right_sum){
+                right_sum=sum;
+                max_right=j;
+            }
+        }
+        return new int[] {max_left,max_right,left_sum+right_sum};
+    }
 
+    public static int[] FIND_MAXIMUM_SUBARRAY(int[] A,int low, int high){
+        if(high==low){
+            return new int[] {low,high,A[low]};
+        }
+        else{
+            int mid=(int)Math.floor((double) (low + high) /2);
+            int[] temp_left=FIND_MAXIMUM_SUBARRAY(A, low, mid);
+            int left_low=temp_left[0];
+            int left_high=temp_left[1];
+            int left_sum=temp_left[2];
+            int[] temp_right=FIND_MAXIMUM_SUBARRAY(A, mid+1, high);
+            int right_low=temp_right[0];
+            int right_high=temp_right[1];
+            int right_sum=temp_right[2];
+            int[] temp_mid=FIND_MAX_CROSSING_SUBARRAY(A,low,mid,high);
+            int cross_low=temp_mid[0];
+            int cross_high=temp_mid[1];
+            int cross_sum=temp_mid[2];
+            if(left_sum>=right_sum&&left_sum>=cross_sum){
+                return new int[] {left_low,left_high,left_sum};
+            }
+            else if(right_sum>=left_sum&&right_sum>=cross_sum){
+                return new int[] {right_low,right_high,right_sum};
+            }
+            else{
+                return new int[] {cross_low,cross_high,cross_sum};
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] A={13,-3,-25,20,-3,-16,-23,18,20,-7,12,-5,-22,15,-4,7};
+        int[] result=FIND_MAXIMUM_SUBARRAY(A,0,A.length-1);
+        System.out.println(result[0]+" "+result[1]+" "+result[2]);
+    }
 }
---
+++ out/production/ITA-Java/README.md
@@ -0,0 +1,8 @@
+# ITA-Java
+Introduction to Algorithms Third Edition, Java implementation.《算法导论》第三版Java实现。
+
+弄完Java实现是个漫长的过程，不知道要多久吧。
+
+反正还是老样子，我会标注出为了适应Java代码，而与原伪代码相比做出的改动。
+
+加油吧。
